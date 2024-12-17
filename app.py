e851d210ad67a40b6f87ce8e770969b78d58803b
West-brick <87601006+West-brick@users.noreply.github.com>
2024-12-17 20:35:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, send_from_directory
 
 app = Flask(__name__, static_folder='.')
@@ -7,4 +8,5 @@ def serve_index():
     return send_from_directory('.', 'index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get('PORT', 8080))  # 监听 $PORT 环境变量
+    app.run(host='0.0.0.0', port=port)
